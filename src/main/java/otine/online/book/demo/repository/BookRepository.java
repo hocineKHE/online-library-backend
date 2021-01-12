ffe7a169aacb1adcf3d8fe6098f123a655d0c93f
Hocine KHEDDADJI <khedadjihoucine@gmail.com>
2021-01-12 15:40:46 +0100

    solve Cross origin problem

--- src/main/java/otine/online/book/demo/repository/BookRepository.java
+++ src/main/java/otine/online/book/demo/repository/BookRepository.java
@@ -2,7 +2,9 @@ package otine.online.book.demo.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import otine.online.book.demo.entity.Book;
 
+@CrossOrigin("*")
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
