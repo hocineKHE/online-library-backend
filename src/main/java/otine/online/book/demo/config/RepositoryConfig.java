34c611b0edf69f8fbe2c0ff29278a736df392f94
Hocine KHEDDADJI <khedadjihoucine@gmail.com>
2021-01-14 10:28:37 +0100

    cors configuration

--- src/main/java/otine/online/book/demo/config/RepositoryConfig.java
+++ src/main/java/otine/online/book/demo/config/RepositoryConfig.java
@@ -24,7 +24,8 @@ public class RepositoryConfig implements RepositoryRestConfigurer {
         config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream()
                 .map(Type::getJavaType)
                 .toArray(Class[]::new)
-        );
 
+        );
+            cors.addMapping("/**").allowedOrigins("http://localhost:4200");
     }
 }
--- src/main/java/otine/online/book/demo/repository/BookRepository.java
+++ src/main/java/otine/online/book/demo/repository/BookRepository.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import otine.online.book.demo.entity.Book;
 
 
-@CrossOrigin("*")
+
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     @RestResource(path = "categoryid")
